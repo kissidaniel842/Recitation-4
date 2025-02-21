7e888e65c84a570a1090f89baed1a8a3038a1271
kissidaniel842 <kissidaniel842@gmail.com>
2025-02-21 09:47:12 -0500

    Done!

--- src/main/java/Recitation4.java
+++ src/main/java/Recitation4.java
@@ -1,24 +1,33 @@
 public class Recitation4 {
 
-    public static String explode(String word){
+    public static String explode(String word) {
         StringBuilder sb = new StringBuilder();
         int duplicateValue;
-        for(int index = 0; index < word.length(); index++){
+        for (int index = 0; index < word.length(); index++) {
             char ch = word.charAt(index);
-            if (Character.isLetter(ch)){
+            if (Character.isLetter(ch)) {
                 duplicateValue = index + 1;
-            } else if (Character.isDigit(ch)){
+            } else if (Character.isDigit(ch)) {
                 duplicateValue = Integer.parseInt("" + ch);
             } else {
-                sb.append(word.substring(0, index));
                 break;
             }
-            for(int count = 0; count < duplicateValue; count++){
+            for (int count = 0; count < duplicateValue; count++) {
                 sb.append(ch);
             }
         }
-        // Return the final result string
+        // Ensure the non-alphanumeric character is added correctly
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (!Character.isLetterOrDigit(ch)) {
+                sb.append(word.substring(0, i));
+                break;
+            }
+        }
         return sb.toString();
     }
 }
+
+
+
 
