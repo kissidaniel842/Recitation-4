108cfb3bcf7fc40987de33f3afe72a5acbb7d29f
kissidaniel842 <kissidaniel842@gmail.com>
2025-02-21 09:25:25 -0500

    Done!

--- src/main/java/Recitation4.java
+++ src/main/java/Recitation4.java
@@ -1,22 +1,26 @@
 public class Recitation4 {
 
-    public static String explode(String word){
+
+    public static String explode(String word) {
         StringBuilder sb = new StringBuilder();
         int duplicateValue;
-        for(int index = 0; index < word.length(); index++){
+
+        for (int index = 0; index < word.length(); index++) {
             char ch = word.charAt(index);
-            if (Character.isLetter(ch)){
-                duplicateValue = index+1;
-            } else if (Character.isDigit(ch)){
-                duplicateValue = Integer.parseInt(""+ch);
+
+            if (Character.isLetter(ch)) {
+                duplicateValue = index + 1;
+            } else if (Character.isDigit(ch)) {
+                duplicateValue = Character.getNumericValue(ch);
             } else {
-                duplicateValue = 0;
-                sb.append(word.substring(0,index));
+                break;  // Stop processing at first non-alphanumeric character
             }
-            for(int count = 0; count < duplicateValue; count++){
+
+            for (int count = 0; count < duplicateValue; count++) {
                 sb.append(ch);
             }
         }
+
         return sb.toString();
     }
 }
